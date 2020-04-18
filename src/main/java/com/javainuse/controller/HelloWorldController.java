68833fb2ebd365771a514db882338538ce6eb209
Rohith <rohitsundaram.95@gmail.com>
2020-04-18 18:56:14 +0530

    First phase of authentication

---
+++ src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.javainuse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping({ "/hello" })
+    public String firstPage() {
+        return "Hello World";
+    }
+    @RequestMapping({ "/hello2" })
+    public String secondPage() {
+        return "Hello Rohit";
+    }
+}
