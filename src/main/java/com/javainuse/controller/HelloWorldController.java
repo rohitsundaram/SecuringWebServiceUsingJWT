1159672de9771a6b02bfd607d753bde34b43d0c5
Rohith <rohitsundaram.95@gmail.com>
2020-04-26 18:59:36 +0530

    Few web service methods are included

--- src/main/java/com/javainuse/controller/HelloWorldController.java
+++ src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.javainuse.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sun.awt.SunHints;
 
 @RestController
 public class HelloWorldController {
@@ -14,4 +17,23 @@ public class HelloWorldController {
     public String secondPage() {
         return "Hello Rohit";
     }
+    @RequestMapping({"/createUser"})
+    public String createUser(@RequestParam(name="userName") String userName,@RequestParam(name="dob")String dob,@RequestParam(name = "profilePic") String profilePic, @RequestParam(name = "location") String location){
+        return "successful/failure"
+    }
+
+    @RequestMapping({"/getOrderHistory"})
+    public String getOrderHistory(@RequestParam(name="userName")String userName){
+
+        return "List of failures";
+    }
+
+    @RequestMapping({"/userInfo"})
+    public String userInfo(@RequestParam(name="userId")String userId){
+
+        return "UserID,Full Name,DOB,ProfilePic,Password,Location,ModeOfPayment";
+    }
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+public void abc(){}
+
 }
